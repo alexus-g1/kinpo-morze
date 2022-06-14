7a8955b573a50d6604aece2e0df2846eaa633086
alexus_g1 <antidot2002@yandex.ru>
2022-06-14 15:26:44 +0300

    init testing project

---
+++ testing-morze/testing-morze.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../kinpo-morze/functions.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testingmorze
+{
+	TEST_CLASS(testingmorze)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
