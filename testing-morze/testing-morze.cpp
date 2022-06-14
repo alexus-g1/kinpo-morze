8e2d447134ecaaf46e6482b9789936833f9665cb
alexus_g1 <antidot2002@yandex.ru>
2022-06-14 15:33:17 +0300

    add test methods to decodeMorze function

--- testing-morze/testing-morze.cpp
+++ testing-morze/testing-morze.cpp
@@ -27,5 +27,76 @@ namespace testingmorze
 	TEST_CLASS(testingmorze)
 	{
 	public:
+		
+		TEST_METHOD(OneDigitTest)
+		{
+			const string InputString = "...--";//3
+			vector<string> InputVector;
+			string BiuldString;
+			decodeMorze(InputString, BiuldString, InputVector);
+
+
+			const vector<string> WaitingResult = {
+				"ЕЕЕТТ","ЕЕЕМ","ЕЕАТ","ЕЕВ","ЕИТТ",
+				"ЕИМ","ЕЮ","ИЕТТ","ИЕМ","ИАТ","ИВ","СТТ","СМ","ЖТ","3"
+			};
+
+			Assert::IsTrue(CompareVectors(InputVector, WaitingResult));
+		}
+
+		TEST_METHOD(OneDoteTest)
+		{
+			const string InputString = ".";//E
+			vector<string> InputVector;
+			string BiuldString;
+			decodeMorze(InputString, BiuldString, InputVector);
+
+			vector<string> WaitingResult = { "Е" };
+			Assert::IsTrue(CompareVectors(InputVector, WaitingResult));
+		}
+
+		TEST_METHOD(OneDashTest)
+		{
+			const string InputString = "-";//T
+			vector<string> InputVector;
+			string BiuldString;
+			decodeMorze(InputString, BiuldString, InputVector);
+
+			vector<string> WaitingResult = { "Т" };
+			Assert::IsTrue(CompareVectors(InputVector, WaitingResult));
+		}
+
+		TEST_METHOD(OneLetterTest)
+		{
+			const string InputString = ".-";//A
+			vector<string> InputVector;
+			string BiuldString;
+			decodeMorze(InputString, BiuldString, InputVector);
+
+			vector<string> WaitingResult = { "ЕТ" ,"А" };
+			Assert::IsTrue(CompareVectors(InputVector, WaitingResult));
+		}
+
+		TEST_METHOD(OneWordTest)
+		{
+			const string InputString = "---..-.---";//ЧАЙ
+			vector<string> InputVector;
+			string BiuldString;
+			decodeMorze(InputString, BiuldString, InputVector);
+
+			string MustBeString = "ЧАЙ";
+			Assert::IsTrue(find(InputVector.begin(), InputVector.end(), MustBeString) != InputVector.end());
+		}
+
+		TEST_METHOD(OneNumberTest)
+		{
+			const string InputString = ".---------";//10
+			vector<string> InputVector;
+			string BiuldString;
+			decodeMorze(InputString, BiuldString, InputVector);
+
+			string MustBeString = "10";
+			Assert::IsTrue(find(InputVector.begin(), InputVector.end(), MustBeString) != InputVector.end());
+		}
 	};
 }
