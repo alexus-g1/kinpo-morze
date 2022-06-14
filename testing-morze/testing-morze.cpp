c5f99df4a9ddfcdf26117cd4ed10b3ae86940161
alexus_g1 <antidot2002@yandex.ru>
2022-06-14 15:31:51 +0300

    add CompareVectors function

--- testing-morze/testing-morze.cpp
+++ testing-morze/testing-morze.cpp
@@ -1,16 +1,31 @@
 ﻿#include "pch.h"
 #include "CppUnitTest.h"
+#include <vector>
+#include <algorithm>
 #include "../kinpo-morze/functions.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+bool CompareVectors(const vector<string>& Checked, const vector<string>& Correct)
+{
+	bool Mark = true;
+	if (Checked.size() != Correct.size()) {
+		Logger::WriteMessage("Different length of vectors!");
+		Mark = false;
+	}
+
+	for (int i = 0; i < Correct.size() && Mark; i++)
+		if (Checked[i] != Correct[i]) {
+			Logger::WriteMessage("Not equal!");
+			Mark = false;
+		}
+
+	return Mark;
+}
 
 namespace testingmorze
 {
 	TEST_CLASS(testingmorze)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
 	};
 }
